995920fac1679a8d8bc8f554ea2a0043d04ea95f
zeero1q <shrikantvishwakrma74@gmail.com>
2021-04-05 11:27:37 +0530

    form element and event handler added. closes #1. closes #2

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ class App extends React.Component {
 
     render() {
         return (
-            <div>
+            <div className="ui container">
                 <SearchBar />
             </div>
         );
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,30 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    state = { term: ''};
+
+    onInputChange = (event) => {
+        this.setState({term: event.target.value});
+    };
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state.term);
+    //    todo: make sure w call callback from parent component
+    };
 
     render() {
         return (
-            <div>
-                From SearchBar
+            <div className="search-bar ui segment">
+                <form onSubmit={this.onFormSubmit} className="ui form">
+                    <div className="field">
+                        <label>Video Search</label>
+                        <input
+                            type="text"
+                            value={this.state.term}
+                            onChange={this.onInputChange}
+                        />
+                    </div>
+                </form>
             </div>
         );
     }
