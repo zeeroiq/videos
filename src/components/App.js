162e2e5d7b25c09eb50ffd164debf13e57f8e17c
zeero1q <shrikantvishwakrma74@gmail.com>
2021-04-05 12:20:13 +0530

    async support and updated state using fetch data

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,15 @@ import SearchBar from "./SearchBar";
 import youtube from '../api/youtube';
 
 class App extends React.Component {
-    onTermSubmit = (term) => {
-        youtube.get('/search', {
+    state = {videos: []};
+    onTermSubmit = async term => {
+        const response = await youtube.get('/search', {
             params: {
                 q: term
             }
         });
+
+        this.setState({videos: response.data.items});
     };
 
     render() {
