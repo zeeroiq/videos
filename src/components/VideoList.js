6074e69d83b1f384a88834c7fe1447cd711a3580
zeero1q <shrikantvishwakrma74@gmail.com>
2021-04-05 12:46:28 +0530

    destructured using es2015 syntax

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-const VideoList = props => {
+const VideoList = ({ videos }) => {
     return (
         <div>
-            {props.videos.length}
+            {videos.length}
         </div>
     );
 };
