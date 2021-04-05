e4ed66456eac3813b85047df6a1dcbee93fd4744
zeero1q <shrikantvishwakrma74@gmail.com>
2021-04-05 13:14:51 +0530

    updated for css styling

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,9 +1,14 @@
 import React from "react";
+import VideoItem from "./VideoItem";
 
 const VideoList = ({ videos }) => {
+    const renderedList = videos.map(video => {
+        return <VideoItem video={video}/>
+    })
+
     return (
-        <div>
-            {videos.length}
+        <div className="ui relaxed divided list">
+            {renderedList}
         </div>
     );
 };
