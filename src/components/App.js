4c6ddb65ef15b1ee79cd3c8e6b33c69a9fb746d7
zeero1q <shrikantvishwakrma74@gmail.com>
2021-04-05 13:34:45 +0530

    added VideoDetail component. closes #5

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import SearchBar from "./SearchBar";
 import youtube from '../api/youtube';
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
-    state = { videos: [], selectedVideo: null };
+    state = {videos: [], selectedVideo: null};
 
     onTermSubmit = async term => {
         const response = await youtube.get('/search', {
@@ -17,12 +18,14 @@ class App extends React.Component {
     };
 
     onVideoSelect = video => {
-        console.info('From the App!', video);
+        this.setState({selectedVideo: video})
     }
+
     render() {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit}/>
+                <VideoDetail video={this.state.selectedVideo}/>
                 <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
             </div>
         );
---
+++ src/components/VideoDetail.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const VideoDetail = ({ video }) => {
+    if (!video) {
+        return <div>Loading...</div>
+    }
+    return (
+        <div>
+            {video.snippet.title}
+        </div>
+    );
+};
+
+export default VideoDetail;
